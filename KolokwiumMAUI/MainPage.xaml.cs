5278516eb983b692356239d4d8696f570b1589fc
613106 <613106@o365.stud.umk.pl>
2023-09-13 12:42:14 +0200

    Add project files.

---
+++ KolokwiumMAUI/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace KolokwiumMAUI;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	public void ZmianaSuwaka(object sender, ValueChangedEventArgs e)
+	{
+		double wartosc = e.NewValue;
+		etykietaWartosc.Text = ((int)wartosc).ToString();
+
+        if (wartosc < 128)
+        {
+            etykietaWartosc.TextColor = Color.FromRgb(255, 0, 0);
+        }
+        else
+        {
+            etykietaWartosc.TextColor = Color.FromRgb(0, 0, 128);
+        }
+	}
+	public void NacisnieciePrzycisku(object sender, EventArgs e)
+	{
+		suwak.Value = 0;
+	}
+}
+
